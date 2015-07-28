20754b1b995cf832eadc5addbfaddddd18a0cf7d
Aarushi <aarushiagarwal1994@gmail.com>
2015-07-28 15:55:33 +0530

    https://github.com/aarushiagarwal1994/Blog.git

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+def after_sign_in_path_for(resource)
+  articles_path(my: true)
+end
+def after_sign_up_path_for(resource)
+  articles_path
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 
  has_many :articles
  has_many :comments
+ validates :email, presence: true, email: true
 end
